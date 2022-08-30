e91602d028f499dfa8c9742e9bb318e4fed9952b
jerineaso <jerin.easo.20@gmail.com>
2022-08-30 15:59:16 +0530

    done till edit and delete is left

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,7 +13,8 @@ import { TodoContainerComponent } from './todo-container/todo-container.componen
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/todo-container/todo-container.component.ts
+++ src/app/todo-container/todo-container.component.ts
@@ -1,42 +1,66 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, NgForm } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-container',
   templateUrl: './todo-container.component.html',
   styleUrls: ['./todo-container.component.css']
 })
-export class TodoContainerComponent implements OnInit {
+export class TodoContainerComponent {
 
   title="todoApp"
-  // Array for storing form data
-  formArray: Array<any> = []
-  constructor() {
-  }
+  todoData!: FormGroup;
+  formData : any = [];
+  isSubmit: boolean = false;
+  data: any = {};
+  isEnabled: boolean = false;
+  isEmpty: boolean = false;
+  index!: number;
+
+
+  constructor() {}
 
   ngOnInit(): void {
-    
-    
+    this.todoData = new FormGroup({
+      id: new FormControl(0),
+      todo_item : new FormControl('', [Validators.required])
+    }) 
   }
 
-  onSubmit(x: NgForm) {
-    if(!x.controls.todo_item.invalid){
-      this.formArray.push({
-        id: this.formArray.length,
-        todo_item:x.value.todo_item
-      })
-    }
+  onSubmit() {
+    this.isSubmit = true;
+    this.todoData.value.id = this.formData.length
+    this.formData.push(this.todoData.value) 
+    console.log(this.formData);
+    console.log()
+    this.todoData.controls['todo_item'].reset();
+  }
+
+  get todo(){
+    return this.todoData.get('todo_item')
   }
 
   edit(x:number){
-    console.log(x);
-    this.formArray.find(item=>{
-      console.log(item);
-      
+    this.data = this.formData.find((item: any) =>{
+      return item.id === x
     })
+    this.index = this.data.id
+    this.todoData.controls['todo_item'].setValue(this.data.todo_item)
+    this.isEnabled = true
+  }
+
+  update(){
+    this.formData[this.index].todo_item = this.todoData.get('todo_item')?.value
+    this.isEnabled = false
   }
 
   delete(x:number){
-    this.formArray.splice(x,1);
+    this.data = this.formData.find((item: any) =>{
+      return item.id === x
+    })
+    
+    this.formData.splice(this.data.id,1)
   }
 }
+
+
